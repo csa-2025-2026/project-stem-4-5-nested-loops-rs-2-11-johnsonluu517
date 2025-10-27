c10dd744f263ce1faa7d4cad495e0fe9bd4b2336
johnsonluu517 <luujohnson07@gmail.com>
2025-10-27 21:46:40 +0000

    working on problem 2

--- Main.java
+++ Main.java
@@ -4,22 +4,35 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Make sure to test your methods here
+    printNums();
   }
 
   public static void printNTimes(String word, int N)
   {
-    // code solution here
+    for (int i = 0; i < word.length(); i++)
+    {
+      String letter = word.substring(i, i+1);
+      for (int j = 0; j < N; j++)
+      {
+        System.out.print(letter);
+      }
+      System.out.println();
+    }
   }
 
   public static void printNums()
   {
-    // code solution here
+    for (int i = 10; i >= 0; i--)
+    {
+       for (int j = i; j >= 0; j--)
+        System.out.print(j);
+    } // code solution here
+    System.out.println();
   }
 
   public static void uprightNumberTriangle(int N)
   {
-    // code solution here
+   
   }
 
   public static void starTree()
