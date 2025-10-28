cead79e701ec903791c0618e9967432f0669c1c7
johnsonluu517 <luujohnson07@gmail.com>
2025-10-28 21:46:24 +0000

    finished problem 2 and 3, working on problem 4

--- Main.java
+++ Main.java
@@ -4,7 +4,12 @@ public class Main
 {
   public static void main(String[] args)
   {
-    printNums();
+    Scanner scan = new Scanner(System.in);
+      System.out.println("Enter a number");
+      int num = scan.nextInt();
+      uprightNumberTriangle(num);
+      
+      starTree();
   }
 
   public static void printNTimes(String word, int N)
@@ -22,22 +27,43 @@ public class Main
 
   public static void printNums()
   {
-    for (int i = 10; i >= 0; i--)
+    for (int i = 10; i > 0; i--)
     {
-       for (int j = i; j >= 0; j--)
-        System.out.print(j);
-    } // code solution here
-    System.out.println();
+       for (int j = i; j > 0; j--)
+        {
+          System.out.print(i);
+
+        }
+         System.out.println();
+    } 
   }
 
   public static void uprightNumberTriangle(int N)
   {
-   
+    for (int row = 1; row <= N+1; row++)
+      {
+        for (int count = 1; count < row; count++)
+        System.out.print(count + " ");
+        System.out.print("\n");
+      }
   }
 
   public static void starTree()
   {
-    // code solution here
+    String space = " ";
+    for (int i = 9; i >=1; i--)
+      {
+        for (int j = i; j >= 1; j--)
+          {
+            for (int controller = 10; controller < 10; controller--)
+            {
+              space += " ";
+            }
+            System.out.print(space +"*" + " ");
+          }
+
+            System.out.print("\n");
+      }
   }
 
   public static void multTable()
